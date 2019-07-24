0253b8ff06a8de41ee17d9cf5aa7c1a804fbe5e9
leighd2008 <leighd2008@gmail.com>
2019-07-24 11:10:22 -0400

    Updates

---
+++ precache-manifest.b8c359def141f94678077592335b7604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbb7c967b57fe5d0d4fd440baec5db4",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "937bb2f5db1570107110",
+    "url": "/mosters-rolodex/static/css/main.5d5a80b7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/mosters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "937bb2f5db1570107110",
+    "url": "/mosters-rolodex/static/js/main.158fd3e8.chunk.js"
+  },
+  {
+    "revision": "253f6367d1a1bea6b3cd",
+    "url": "/mosters-rolodex/static/js/runtime~main.7c72627f.js"
+  }
+]);
